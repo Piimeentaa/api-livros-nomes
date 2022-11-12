60850659736148c807954a0d57ac22d69ea72b03
Piimeentaa <dora13.isa@gmail.com>
2022-11-12 00:32:51 -0300

    configs

---
+++ nomesLivros/src/main/java/br/com/isamaceio/nomesLivros/NomesLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.isamaceio.nomesLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomesLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomesLivrosApplication.class, args);
+	}
+
+}
---
+++ nomesLivros/src/test/java/br/com/isamaceio/nomesLivros/NomesLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isamaceio.nomesLivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomesLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
